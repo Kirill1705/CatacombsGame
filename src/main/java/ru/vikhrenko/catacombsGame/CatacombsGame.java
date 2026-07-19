10a3e0ab7db36e0d4d4a2b9b9bd712014c286a15
Kirill <vikhrenko.kirill@gmail.com>
2026-07-19 16:33:02 +0300

    init

---
+++ src/main/java/ru/vikhrenko/catacombsGame/CatacombsGame.java
@@ -0,0 +1,16 @@
+package ru.vikhrenko.catacombsGame;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CatacombsGame extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
